3e9e6a2b542cf8965fc11e88c6731e9954178de8
ragalearn <ragapangestu55@gmail.com>
2026-01-16 00:51:12 +0700

    feat: add social login (Google, Facebook, Apple) with OAuth2 using Laravel Socialite

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,42 +5,68 @@
         <!-- Name -->
         <div>
             <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+            <x-text-input
+                id="name"
+                class="block mt-1 w-full"
+                type="text"
+                name="name"
+                :value="old('name')"
+                required
+                autofocus
+                autocomplete="name"
+            />
             <x-input-error :messages="$errors->get('name')" class="mt-2" />
         </div>
 
-        <!-- Email Address -->
+        <!-- Email -->
         <div class="mt-4">
             <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
+            <x-text-input
+                id="email"
+                class="block mt-1 w-full"
+                type="email"
+                name="email"
+                :value="old('email')"
+                required
+                autocomplete="username"
+            />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
         <!-- Password -->
         <div class="mt-4">
             <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="new-password" />
-
+            <x-text-input
+                id="password"
+                class="block mt-1 w-full"
+                type="password"
+                name="password"
+                required
+                autocomplete="new-password"
+            />
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
 
         <!-- Confirm Password -->
         <div class="mt-4">
             <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                            type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
-
+            <x-text-input
+                id="password_confirmation"
+                class="block mt-1 w-full"
+                type="password"
+                name="password_confirmation"
+                required
+                autocomplete="new-password"
+            />
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
 
         <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
+            <a
+                class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md
+                       focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                href="{{ route('login') }}"
+            >
                 {{ __('Already registered?') }}
             </a>
 
@@ -49,4 +75,26 @@
             </x-primary-button>
         </div>
     </form>
+
+    <!-- SOCIAL REGISTER -->
+    <hr class="my-4">
+
+    <p class="text-sm text-gray-600 mb-2">Atau daftar menggunakan:</p>
+
+    <div class="flex flex-col gap-2">
+        <a href="{{ url('/auth/google') }}"
+           class="px-4 py-2 border rounded-md hover:bg-gray-100 text-center">
+            Daftar dengan Google
+        </a>
+
+        <a href="{{ url('/auth/facebook') }}"
+           class="px-4 py-2 border rounded-md hover:bg-gray-100 text-center">
+            Daftar dengan Facebook
+        </a>
+
+        <a href="{{ url('/auth/apple') }}"
+           class="px-4 py-2 border rounded-md hover:bg-gray-100 text-center">
+            Daftar dengan Apple
+        </a>
+    </div>
 </x-guest-layout>
